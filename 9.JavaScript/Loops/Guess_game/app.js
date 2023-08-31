33ab43452d517a385741ee1f32616cfd6b3f8478
Jinnie <hjk7639@gmail.com>
2023-08-31 13:59:06 +0900

    JS - Loops _ Guess game final good working code

--- 9.JavaScript/Loops/Guess_game/app.js
+++ 9.JavaScript/Loops/Guess_game/app.js
@@ -7,18 +7,23 @@ while (!maximum) {
 
 const targetNumber = Math.floor(Math.random() * maximum) + 1;
 
+// user has already made their one guess at this point, so set 1.
 let attemps = 1;
 
-let guess = parseInt(prompt('Enter your first guess!'));
+let guess = prompt("Enter your first guess!(Type 'q' to quit the game)");
 
 while (parseInt(guess) !== targetNumber) {
   if (guess === 'q') break;
+  guess = parseInt(guess);
   // user have made it into this loop means that users are guessing again.
-  attemps++;
   if (guess < targetNumber) {
     guess = prompt('Too Low: guess again!');
+    attemps++;
   } else if (guess > targetNumber) {
     guess = prompt('Too High: guess again!');
+    attemps++;
+  } else {
+    guess = prompt('Invalid guess. Please enter a number or "q" to quit');
   }
 }
 
