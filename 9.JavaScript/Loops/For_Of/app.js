29e3f148d9be774d4b7d1c9a5691f7ffd29c4aef
Jinnie <hjk7639@gmail.com>
2023-08-31 15:12:08 +0900

    JS - Loops - for of

---
+++ 9.JavaScript/Loops/For_Of/app.js
@@ -0,0 +1,9 @@
+const bingeWatchingLists = ['YOU', 'HeartStopper', 'YoungRoyals', 'RedandWhite,RoyalBlue', 'Suits'];
+
+// for (let i = 0; i < bingeWatchingLists.length; i++) {
+//   console.log(`You should watch ${bingeWatchingLists[i]}!! 🤩`);
+// }
+
+for (let list of bingeWatchingLists) {
+  console.log(`You should watch ${list}!! 🤩`);
+}
