20cc1e734f62dabd7a134df4f4041f90f70726be
Jinnie <hjk7639@gmail.com>
2023-09-01 08:40:23 +0900

    JS - Scope _ Functional Scope

---
+++ 9.JavaScript/Function/Levelup/app.js
@@ -0,0 +1,21 @@
+// function collectEggs() {
+//   let totalEggs = 6; // internal variable (most common)
+// }
+// console.log(totalEggs);
+
+// let totalEggs = 0; // global variable
+// function collectEggs() {
+//   totalEggs = 6;
+// }
+// console.log(totalEggs); // 0
+// collectEggs();
+// console.log(totalEggs); // 6
+
+let bird = 'Scarlet Macaw';
+
+function birdWatch() {
+  // let bird = 'Great Blue Heron';
+  console.log(bird);
+}
+birdWatch();
+// console.log(bird);
