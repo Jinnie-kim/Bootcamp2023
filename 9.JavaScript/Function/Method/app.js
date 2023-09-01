9f93f295ded58d937fc93b2592d04afbafb937bb
Jinnie <hjk7639@gmail.com>
2023-09-01 22:12:39 +0900

    JS - This

--- 9.JavaScript/Function/Method/app.js
+++ 9.JavaScript/Function/Method/app.js
@@ -7,3 +7,17 @@ const myMath = {
     return num ** 3;
   },
 };
+
+// this
+
+const cat = {
+  name: 'Blue Steele',
+  color: 'grey',
+  breed: 'scottish fold',
+  meow() {
+    console.log('This is:', this);
+    console.log(`${this.name} says MEOW`);
+  },
+};
+
+const meow2 = cat.meow;
