74872b37575421e7db77746b26fa09bf6ed42b15
Jinnie <hjk7639@gmail.com>
2023-09-03 20:26:45 +0900

    JS - ForEach

---
+++ 9.JavaScript/Callback&Array/app.js
@@ -0,0 +1,28 @@
+// ForEach
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
+function print(element) {
+  console.log(element);
+}
+
+numbers.forEach(function (element) {
+  if (element % 2 === 0) {
+    console.log(element);
+  }
+});
+
+// easier to read, write shorter
+for (let el of numbers) {
+  console.log(el);
+}
+
+const movies = [
+  { title: 'Amadeus', score: 99 },
+  { title: 'Stand By Me', score: 85 },
+  { title: 'Parasite', score: 95 },
+  { title: 'Alien', score: 90 },
+];
+
+movies.forEach((movie) => {
+  console.log(`${movie.title} - ${movie.score}/100`);
+});
