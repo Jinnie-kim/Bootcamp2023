e9160e3837ada2e6fc66a57fd39a976c794b0409
Jinnie <hjk7639@gmail.com>
2023-09-09 11:50:24 +0900

    JS - DOM Events - Score Keeper practice 2

--- 10.DOM/Events/scoreKeeper/app.js
+++ 10.DOM/Events/scoreKeeper/app.js
@@ -6,44 +6,53 @@ const player2Button = document.querySelector('#player2');
 const resetButton = document.querySelector('#reset');
 
 let winningScore;
-
 let player1ScoreNumber = 0;
 let player2ScoreNumber = 0;
+let isGameOver = false;
 
 setSelect.addEventListener('change', function () {
   winningScore = Number(setSelect.value);
 });
 
 player1Button.addEventListener('click', function () {
-  player1ScoreNumber += 1;
-  player1Score.textContent = player1ScoreNumber;
+  if (!isGameOver) {
+    player1ScoreNumber += 1;
+    if (player1ScoreNumber === winningScore) {
+      isGameOver = true;
+      player1Button.disabled = true;
+      player2Button.disabled = true;
+      player1Score.classList.add('success');
+      player2Score.classList.add('lose');
+    }
+    player1Score.textContent = player1ScoreNumber;
+  }
 });
 
 player2Button.addEventListener('click', function () {
-  player2ScoreNumber += 1;
-  player2Score.textContent = player2ScoreNumber;
+  if (!isGameOver) {
+    player2ScoreNumber += 1;
+    if (player2ScoreNumber === winningScore) {
+      isGameOver = true;
+      player1Button.disabled = true;
+      player2Button.disabled = true;
+      player2Score.classList.add('success');
+      player1Score.classList.add('lose');
+    }
+    player2Score.textContent = player2ScoreNumber;
+  }
 });
 
-// if (Number(player1Score.textContent) + Number(player2Score.textContent) === setNumber) {
-//   player1Button.disabled = true;
-//   player2Button.disabled = true;
+resetButton.addEventListener('click', reset);
 
-//   if (Number(player1Score.textContent) > Number(player2Score.textContent)) {
-//     player1Score.style.color = 'green';
-//     player2Score.style.color = 'red';
-//   } else {
-//     player1Score.style.color = 'red';
-//     player2Score.style.color = 'green';
-//   }
-// }
-
-resetButton.addEventListener('click', function () {
+function reset() {
+  isGameOver = false;
   player1Button.disabled = false;
   player2Button.disabled = false;
-
-  player1Score.style.color = 'black';
-  player2Score.style.color = 'black';
-
   player1Score.textContent = 0;
   player2Score.textContent = 0;
-});
+  player1ScoreNumber = 0;
+  player2ScoreNumber = 0;
+  player2Score.classList.remove('success', 'lose');
+  player1Score.classList.remove('success', 'lose');
+  setSelect.value = '';
+}
