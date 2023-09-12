15694308ba77cc51c2b544682d5c9a1b6397038f
Jinnie <hjk7639@gmail.com>
2023-09-12 14:18:25 +0900

    JS - Async - await keyword

--- 11.AsyncJS/AsyncFunc/app.js
+++ 11.AsyncJS/AsyncFunc/app.js
@@ -25,3 +25,40 @@ login('sfsdf', 'corgifeetarecute')
     console.log('ERROR');
     console.log(err);
   });
+
+const delayedColorChange = (color, delay) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      document.body.style.backgroundColor = color;
+      resolve();
+    }, delay);
+  });
+};
+
+// delayedColorChange('red', 1000)
+//   .then(() => delayedColorChange('orange', 1000))
+//   .then(() => delayedColorChange('yellow', 1000))
+//   .then(() => delayedColorChange('green', 1000))
+//   .then(() => delayedColorChange('blue', 1000))
+//   .then(() => delayedColorChange('indigo', 1000));
+
+async function rainbow() {
+  await delayedColorChange('red', 1000);
+  await delayedColorChange('orange', 1000);
+  await delayedColorChange('yellow', 1000);
+  await delayedColorChange('green', 1000);
+  await delayedColorChange('blue', 1000);
+  await delayedColorChange('indigo', 1000);
+  await delayedColorChange('pruple', 1000);
+  return 'All Done ';
+}
+
+// rainbow().then(() => console.log('END OF the Rainbow'));
+
+// same with right above function
+async function printRainbow() {
+  await rainbow();
+  console.log('END OF the Rainbow');
+}
+
+printRainbow();
