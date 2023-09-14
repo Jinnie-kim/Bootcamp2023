aed22dd68f1959c5fc110586c2b4b93a9d2d03fd
Jinnie <hjk7639@gmail.com>
2023-09-14 09:59:13 +0900

    JS - Async - Acxios practice

--- 12.AJAX&API/Axios/app.js
+++ 12.AJAX&API/Axios/app.js
@@ -16,5 +16,26 @@ const getStarWarsPerson = async (id) => {
   }
 };
 
-getStarWarsPerson(1);
-getStarWarsPerson(2);
+const jokeList = document.querySelector('#jokes');
+const button = document.querySelector('button');
+
+const getDadJoke = async () => {
+  try {
+    const config = { headers: { Accept: 'application/json' } };
+    const res = await axios.get('https://icanhazdadjoke.com/', config);
+
+    return res.data.joke;
+  } catch (e) {
+    return 'NO JOKES AVAILABLE! SORRY';
+  }
+};
+
+const addNewJoke = async () => {
+  const jokeText = await getDadJoke();
+  const li = document.createElement('li');
+  li.innerText = jokeText;
+
+  jokeList.appendChild(li);
+};
+
+button.addEventListener('click', addNewJoke);
