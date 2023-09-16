e2537856f04bc29ee67ed34c30139b59b05d4630
Jinnie <hjk7639@gmail.com>
2023-09-16 21:11:13 +0900

    JS - module exports & require

---
+++ 14.NPM&Module/exports/app.js
@@ -0,0 +1,6 @@
+const { PI, square } = require('./math');
+console.log(PI);
+console.log(square(6));
+
+const cats = require('../shelter/index');
+console.log('Required an entire directory', cats);
---
+++ 14.NPM&Module/exports/math.js
@@ -0,0 +1,20 @@
+module.exports.add = (x, y) => x + y;
+
+module.exports.PI = 3.14159;
+
+module.exports.square = (x) => x * x;
+
+// ========================
+// const math = {
+//   add: add,
+//   PI: PI,
+//   square: square,
+// };
+
+// module.exports = math;
+// module.exports.add = add;
+// module.exports.PI = PI;
+// module.exports.square = square;
+
+// shortcut syntax
+// exports.PI = PI;
