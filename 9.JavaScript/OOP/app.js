810a3571076114817b0552b4fc40ab3958cc6b15
Jinnie <hjk7639@gmail.com>
2023-09-16 10:56:55 +0900

    JS - OOP - More Class

--- 9.JavaScript/OOP/app.js
+++ 9.JavaScript/OOP/app.js
@@ -81,6 +81,7 @@ class Color {
     this.g = g;
     this.b = b;
     this.name = name;
+    this.calcHSL();
   }
 
   innerRGB() {
@@ -100,7 +101,68 @@ class Color {
   rgba(a = 1) {
     return `rgba(${this.innerRGB()}, ${a})`;
   }
+
+  hsl() {
+    const { h, s, l } = this;
+    return `hsl(${h}, ${s}%, ${l}%)`;
+  }
+
+  fullySaturated() {
+    const { h, l } = this;
+    return `hsl(${h}, 100%, ${l}%)`;
+  }
+
+  opposite() {
+    const { h, s, l } = this;
+    const newHue = (h + 180) % 360;
+    return `hsl(${newHue}, ${s}%, ${l}%)`;
+  }
+
+  calcHSL() {
+    let { r, g, b } = this;
+
+    // Make r, g, and b fractions of 1
+    r /= 255;
+    g /= 255;
+    b /= 255;
+
+    // Find greatest and smallest channel values
+    let cmin = Math.min(r, g, b),
+      cmax = Math.max(r, g, b),
+      delta = cmax - cmin,
+      h = 0,
+      s = 0,
+      l = 0;
+    if (delta == 0) h = 0;
+    else if (cmax == r)
+      // Red is max
+      h = ((g - b) / delta) % 6;
+    else if (cmax == g)
+      // Green is max
+      h = (b - r) / delta + 2;
+    // Blue is max
+    else h = (r - g) / delta + 4;
+
+    h = Math.round(h * 60);
+
+    // Make negative hues positive behind 360°
+    if (h < 0) h += 360;
+    // Calculate lightness
+    l = (cmax + cmin) / 2;
+
+    // Calculate saturation
+    s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+
+    // Multiply l and s by 100
+    s = +(s * 100).toFixed(1);
+    l = +(l * 100).toFixed(1);
+
+    this.h = h;
+    this.s = s;
+    this.l = l;
+  }
 }
 
 const spinach = new Color(40, 255, 60, 'spinach');
-const tomato = new Color(0, 255, 60, 'tomato');
+const tomato = new Color(255, 67, 89, 'tomato');
+const orange = new Color(230, 126, 34, 'carrot');
