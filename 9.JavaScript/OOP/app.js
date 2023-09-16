d9d69a75ab29e02e8fb5f7eb4acb54ab75d465d6
Jinnie <hjk7639@gmail.com>
2023-09-16 11:28:57 +0900

    JS - OOP - Extends and Super keyword

--- 9.JavaScript/OOP/app.js
+++ 9.JavaScript/OOP/app.js
@@ -75,94 +75,126 @@ function rgb(r, g, b) {
 
 // Class
 
-class Color {
-  constructor(r, g, b, name) {
-    this.r = r;
-    this.g = g;
-    this.b = b;
+// class Color {
+//   constructor(r, g, b, name) {
+//     this.r = r;
+//     this.g = g;
+//     this.b = b;
+//     this.name = name;
+//     this.calcHSL();
+//   }
+
+//   innerRGB() {
+//     const { r, g, b } = this;
+//     return `${r}, ${g}, ${b}`;
+//   }
+
+//   rgb() {
+//     return `rgb(${this.innerRGB()})`;
+//   }
+
+//   hex() {
+//     const { r, g, b } = this;
+//     return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+//   }
+
+//   rgba(a = 1) {
+//     return `rgba(${this.innerRGB()}, ${a})`;
+//   }
+
+//   hsl() {
+//     const { h, s, l } = this;
+//     return `hsl(${h}, ${s}%, ${l}%)`;
+//   }
+
+//   fullySaturated() {
+//     const { h, l } = this;
+//     return `hsl(${h}, 100%, ${l}%)`;
+//   }
+
+//   opposite() {
+//     const { h, s, l } = this;
+//     const newHue = (h + 180) % 360;
+//     return `hsl(${newHue}, ${s}%, ${l}%)`;
+//   }
+
+//   calcHSL() {
+//     let { r, g, b } = this;
+
+//     // Make r, g, and b fractions of 1
+//     r /= 255;
+//     g /= 255;
+//     b /= 255;
+
+//     // Find greatest and smallest channel values
+//     let cmin = Math.min(r, g, b),
+//       cmax = Math.max(r, g, b),
+//       delta = cmax - cmin,
+//       h = 0,
+//       s = 0,
+//       l = 0;
+//     if (delta == 0) h = 0;
+//     else if (cmax == r)
+//       // Red is max
+//       h = ((g - b) / delta) % 6;
+//     else if (cmax == g)
+//       // Green is max
+//       h = (b - r) / delta + 2;
+//     // Blue is max
+//     else h = (r - g) / delta + 4;
+
+//     h = Math.round(h * 60);
+
+//     // Make negative hues positive behind 360°
+//     if (h < 0) h += 360;
+//     // Calculate lightness
+//     l = (cmax + cmin) / 2;
+
+//     // Calculate saturation
+//     s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+
+//     // Multiply l and s by 100
+//     s = +(s * 100).toFixed(1);
+//     l = +(l * 100).toFixed(1);
+
+//     this.h = h;
+//     this.s = s;
+//     this.l = l;
+//   }
+// }
+
+// const spinach = new Color(40, 255, 60, 'spinach');
+// const tomato = new Color(255, 67, 89, 'tomato');
+// const orange = new Color(230, 126, 34, 'carrot');
+
+class Pet {
+  constructor(name, age) {
+    console.log('in Pet Constructor');
+
     this.name = name;
-    this.calcHSL();
+    this.age = age;
   }
-
-  innerRGB() {
-    const { r, g, b } = this;
-    return `${r}, ${g}, ${b}`;
+  eat() {
+    return `${this.name} is eating...!!!`;
   }
-
-  rgb() {
-    return `rgb(${this.innerRGB()})`;
+}
+class Cat extends Pet {
+  constructor(name, age, livesLeft = 9) {
+    console.log('in Cat Constructor');
+    super(name, age);
+    this.livesLeft = livesLeft;
   }
-
-  hex() {
-    const { r, g, b } = this;
-    return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
-  }
-
-  rgba(a = 1) {
-    return `rgba(${this.innerRGB()}, ${a})`;
-  }
-
-  hsl() {
-    const { h, s, l } = this;
-    return `hsl(${h}, ${s}%, ${l}%)`;
-  }
-
-  fullySaturated() {
-    const { h, l } = this;
-    return `hsl(${h}, 100%, ${l}%)`;
-  }
-
-  opposite() {
-    const { h, s, l } = this;
-    const newHue = (h + 180) % 360;
-    return `hsl(${newHue}, ${s}%, ${l}%)`;
-  }
-
-  calcHSL() {
-    let { r, g, b } = this;
-
-    // Make r, g, and b fractions of 1
-    r /= 255;
-    g /= 255;
-    b /= 255;
-
-    // Find greatest and smallest channel values
-    let cmin = Math.min(r, g, b),
-      cmax = Math.max(r, g, b),
-      delta = cmax - cmin,
-      h = 0,
-      s = 0,
-      l = 0;
-    if (delta == 0) h = 0;
-    else if (cmax == r)
-      // Red is max
-      h = ((g - b) / delta) % 6;
-    else if (cmax == g)
-      // Green is max
-      h = (b - r) / delta + 2;
-    // Blue is max
-    else h = (r - g) / delta + 4;
-
-    h = Math.round(h * 60);
-
-    // Make negative hues positive behind 360°
-    if (h < 0) h += 360;
-    // Calculate lightness
-    l = (cmax + cmin) / 2;
-
-    // Calculate saturation
-    s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
-
-    // Multiply l and s by 100
-    s = +(s * 100).toFixed(1);
-    l = +(l * 100).toFixed(1);
-
-    this.h = h;
-    this.s = s;
-    this.l = l;
+  meow() {
+    return 'MEOWWW!!';
   }
 }
 
-const spinach = new Color(40, 255, 60, 'spinach');
-const tomato = new Color(255, 67, 89, 'tomato');
-const orange = new Color(230, 126, 34, 'carrot');
+class Dog extends Pet {
+  bark() {
+    return 'Wooof!';
+  }
+
+  eat() {
+    return `${this.name} scarfs his food`;
+  }
+}
