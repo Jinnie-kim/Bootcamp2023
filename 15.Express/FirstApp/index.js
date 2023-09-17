570cbc5bc04e944a41594c7ac5e8701c5653f989
Jinnie <hjk7639@gmail.com>
2023-09-17 22:20:19 +0900

    JS - Express route method

--- 15.Express/FirstApp/index.js
+++ 15.Express/FirstApp/index.js
@@ -2,11 +2,34 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res) => {
-  console.log('We got a new request');
-  // res.send('HELLO, We got your request. this is a response');
-  // res.send({ color: 'red' });
-  res.send('<h1>This is my web page!</h1>');
+// app.use((req, res) => {
+//   console.log('We got a new request');
+//   res.send('HELLO, We got your request. this is a response');
+//   res.send({ color: 'red' });
+//   res.send('<h1>This is my web page!</h1>');
+// });
+
+// /cats => 'meow'
+// /dogs => 'woof'
+// '/'
+
+app.get('/cats', (req, res) => {
+  res.send('MEOW!!');
+});
+
+app.get('/dogs', (req, res) => {
+  res.send('WOOF!!');
+});
+
+// root route
+app.get('/', (req, res) => {
+  res.send('This is the home page');
+});
+
+// This code come at the end
+// If I write this code up top, all of other method will be ignored.
+app.get('*', (req, res) => {
+  res.send("I don't know that path");
 });
 
 app.listen(3000, () => {
