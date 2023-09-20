2b31bd3e076232e92a9878315a6912800d902e01
Jinnie <hjk7639@gmail.com>
2023-09-20 10:37:00 +0900

    JS - Express partial & ejs include

--- 16.Templating/Bootstrap_demo/index.js
+++ 16.Templating/Bootstrap_demo/index.js
@@ -24,7 +24,7 @@ app.get('/r/:subreddit', (req, res) => {
 
 app.get('/rand', (req, res) => {
   const num = Math.floor(Math.random() * 10) + 1;
-  res.render('random', { rand: num });
+  res.render('random', { num });
 });
 
 app.get('/cats', (req, res) => {
