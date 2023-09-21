4f8702804c37b4b7351903ccd61eb9aa1964c3c9
Jinnie <hjk7639@gmail.com>
2023-09-21 21:12:41 +0900

    JS - RESTful ~ DELETE

--- 17.RESTfulRoute/Restdemo/index.js
+++ 17.RESTfulRoute/Restdemo/index.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const methodOverride = require('method-override');
 const { v4: uuid } = require('uuid');
 uuid();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(methodOverride('_method'));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 
-const comments = [
+let comments = [
   {
     id: uuid(),
     username: 'Todd',
@@ -47,12 +49,25 @@ app.get('/comments/:id', (req, res) => {
   res.render('comments/show', { comment });
 });
 
+app.get('/comments/:id/edit', (req, res) => {
+  const { id } = req.params;
+  const comment = comments.find((c) => c.id === id);
+  res.render('comments/edit', { comment });
+});
+
 // Update out of CRUD
 app.patch('/comments/:id', (req, res) => {
   const { id } = req.params;
   const newCommentText = req.body.comment;
   const foundComment = comments.find((c) => c.id === id);
   foundComment.comment = newCommentText;
+  res.redirect('/comments');
+});
+
+// Delete out of CRUD
+app.delete('/comments/:id', (req, res) => {
+  const { id } = req.params;
+  comments = comments.filter((comment) => comment.id !== id);
   res.redirect('/comments');
 });
 
