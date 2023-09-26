f83322c27f9dd31846459adaa67c3592fca1e43b
Jinnie <hjk7639@gmail.com>
2023-09-26 22:16:09 +0900

    JS - Mongoose ~ middleware

---
+++ 18.MongooseBasics/person.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+// mongoose.connect('mongodb://127.0.0.1:27017/movieApp');
+
+main().catch((err) => console.log(err));
+
+async function main() {
+  mongoose.connect('mongodb://127.0.0.1:27017/shopApp');
+  console.log('CONNECTION OPEN!!!');
+}
+
+const personSchema = new mongoose.Schema({
+  first: String,
+  last: String,
+});
+
+personSchema.virtual('fullName').get(function () {
+  return `${this.first} ${this.last}`;
+});
+
+personSchema.pre('save', async function () {
+  this.first = 'Yo';
+  this.last = 'MAMA';
+  console.log('ABOUT TO SAVE!!!');
+});
+
+personSchema.post('save', async function () {
+  console.log('JUST SAVED!!!');
+});
+
+const Person = mongoose.model('Person', personSchema);
