6ea7bdc99de502f1d4bf58e60b83dd69217f2e72
Jinnie <hjk7639@gmail.com>
2023-10-06 15:04:08 +0900

    JS - Yelp Camp : add Image

--- 20.YelpCamp/seeds/index.js
+++ 20.YelpCamp/seeds/index.js
@@ -30,9 +30,14 @@ const seedDB = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
+      image: 'https://random.imagecdn.app/500/150',
+      description:
+        'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores, amet. Beatae odio perspiciatis iusto eum nostrum, deserunt, sit deleniti ut enim et natus maiores animi, laboriosam vel molestias libero? Debitis.',
+      price,
     });
     await camp.save();
   }
