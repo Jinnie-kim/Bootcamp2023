8b51f2b7b9ad7767b7bd427c4bb27f18fe3723e0
Jinnie <hjk7639@gmail.com>
2023-10-13 11:45:44 +0900

    JS - Mongo relationships with Express: delete using mongoose middleware

--- 24.DemoApp/models/farm.js
+++ 24.DemoApp/models/farm.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Product = require('./product');
 const { Schema } = mongoose;
 
 const farmSchema = new Schema({
@@ -19,6 +20,14 @@ const farmSchema = new Schema({
       ref: 'Product',
     },
   ],
+});
+
+farmSchema.post('findOneAndDelete', async function (farm) {
+  // we do have access to the data
+  if (farm.products.length) {
+    const res = await Product.deleteMany({ _id: { $in: farm.products } });
+    console.log(res);
+  }
 });
 
 const Farm = mongoose.model('Farm', farmSchema);
