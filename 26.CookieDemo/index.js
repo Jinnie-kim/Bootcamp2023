b2b7fc7b0cc7ea7df6e2300a6630fc2aa2aaf8b6
Jinnie <hjk7639@gmail.com>
2023-10-16 11:02:57 +0900

    JS - Express's Router & Cookies : signing cookie

--- 26.CookieDemo/index.js
+++ 26.CookieDemo/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 const cookieParser = require('cookie-parser');
-app.use(cookieParser());
+app.use(cookieParser('thisiismysecret')); // that string is going to be used by cookie parser to sign my cookies
 
 app.get('/greet', (req, res) => {
   const { name = 'Alex' } = req.cookies;
@@ -14,6 +14,17 @@ app.get('/setname', (req, res) => {
   res.cookie('name', 'Henry');
   res.cookie('animal', 'harlequin shrimp');
   res.send('ok sent you a cookie');
+});
+
+app.get('/getsignedcookie', (req, res) => {
+  res.cookie('fruit', 'grape', { signed: true });
+  res.send('ok signed your fruit cookie');
+});
+
+app.get('/verifyfruit', (req, res) => {
+  console.log(req.cookies);
+  console.log(req.signedCookies);
+  res.send(req.signedCookies);
 });
 
 app.listen(3000, () => {
