f80d28c4307c3ce7b0c20888ddc7cebe56fd8036
Jinnie <hjk7639@gmail.com>
2023-10-22 19:43:04 +0900

    JS - Yelp Camp : Authentication - Include the author for each campground post

--- 20.YelpCamp/seeds/index.js
+++ 20.YelpCamp/seeds/index.js
@@ -32,6 +32,7 @@ const seedDB = async () => {
     const random1000 = Math.floor(Math.random() * 1000);
     const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
+      author: '6534b5f292621e048506f34e',
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
       image: 'https://random.imagecdn.app/500/150',
