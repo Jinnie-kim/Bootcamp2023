05a9863e9739ad2874597fa0ca8efc35afeae9ea
Jinnie <hjk7639@gmail.com>
2023-10-22 20:55:28 +0900

    JS - Yelp Camp : Authentication - Reviews permissions

--- 20.YelpCamp/routes/reivew.js
+++ 20.YelpCamp/routes/reivew.js
@@ -8,10 +8,12 @@ const Campground = require('../models/campground');
 
 router.post(
   '/',
+  isLoggedIn,
   validateReview,
   catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     const review = new Review(req.body.review);
+    review.author = req.user._id;
     campground.reviews.push(review);
     await review.save();
     await campground.save();
