6f5930536aa809fb845bfb6700aa46d92bd62b1b
Jinnie <hjk7639@gmail.com>
2023-10-22 15:59:42 +0900

    JS - Yelp Camp : Authentication - set Logout route and nav link

--- 20.YelpCamp/routes/users.js
+++ 20.YelpCamp/routes/users.js
@@ -33,4 +33,13 @@ router.post('/login', passport.authenticate('local', { failureFlash: true, failu
   res.redirect('/campgrounds');
 });
 
+router.get('/logout', (req, res, next) => {
+  req.logout(function (err) {
+    if (err) {
+      next();
+    }
+    req.flash('success', 'Goodbye');
+    return res.redirect('/campgrounds');
+  });
+});
 module.exports = router;
